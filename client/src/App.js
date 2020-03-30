17e568951442e5aff214c52ec137ce4396ab11a8
alvarian <vitarka88@gmail.com>
2020-03-30 13:36:19 -0400

    legacy html and css replicated into home page

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Contact from './pages/Contact.js';
 import Navbar from './pages/components/Navbar.js';
 
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
+import './styles/flexbox.css';
+import './styles/modalbox.css';
 
 function App() {
   return (
