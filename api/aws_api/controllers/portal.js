035d7e39445110ee4511d02c00f73370e10bcec2
alvarian <vitarka88@gmail.com>
2020-08-10 16:51:31 -0400

    read api, local page authentication

---
+++ api/aws_api/controllers/portal.js
@@ -0,0 +1,56 @@
+const db = require('../config/db');
+
+
+const createProject = (req, res) => {
+	const {} = req.body;
+	console.log(req.body);
+	// db.query(`SELECT * FROM public.add_project()`, 
+	// (err, result) => {
+	// 	console.log(err ? err : result.rows);
+
+	// 	res.send(result.rows);
+	// });
+};
+
+const readAllProjects = (req, res) => {
+	db.query(`SELECT * FROM public.find_all()`, (err, result) => {
+		console.log(err ? err : result.rows);
+
+		res.render('portal', result.rows);
+	});
+};
+
+const readOneProject = (req, res) => {
+	db.query(`SELECT * FROM public.find_by_id(${req.params.id})`, (err, result) => {
+		console.log(err ? err : result.rows);
+
+		res.send(result.rows[0]);
+	});
+};
+
+const updateProject = (req, res) => {
+	const {} = req.body;
+	console.log(req.body);
+	// db.query(`SELECT * FROM public.update_project(${req.params.id})`, (err, result) => {
+	// 	console.log(err ? err : result.rows);
+
+	// 	res.send(result.rows);
+	// });
+};
+
+const deleteProject = (req, res) => {
+	console.log(req.params.id);
+	// db.query(`SELECT * FROM public.delete_project(${req.params.id})`, (err, result) => {
+	// 	console.log(err ? err : result.rows);
+
+	// 	res.send(result.rows);
+	// });
+};
+
+
+module.exports = {
+	createProject,
+	readAllProjects, readOneProject,
+	updateProject,
+	deleteProject
+}
