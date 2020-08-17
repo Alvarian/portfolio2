dcdb7ba98c1beca967b89deb4fc7ecc3536a89e3
alvarian <vitarka88@gmail.com>
2020-08-17 14:47:22 -0400

    include aws writing and in success, write to local db

--- api/aws_api/config/s3.js
+++ api/aws_api/config/s3.js
@@ -1,2 +1,47 @@
+const aws = require('aws-sdk');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
+const fs = require('fs');
+
+
 require('dotenv').config();
-console.log(process.env.BUCKET_NAME);
+const { 
+	ACCESS_KEY_ID,
+	ACCESS_SECRET_KEY,
+	REGION_NAME,
+	BUCKET_NAME
+} = process.env;
+
+const s3 = new aws.S3();
+
+aws.config.update({
+	secretAccessKey: ACCESS_SECRET_KEY,
+	accessKeyId: ACCESS_KEY_ID,
+	region: REGION_NAME
+});
+
+
+function upload(file, fileKey) {
+	if (!file) return null;
+
+	return new Promise(async function(resolve, reject) {
+		const params = {
+			Bucket: BUCKET_NAME, // pass your bucket name
+			Key: fileKey,
+			ACL: 'public-read',
+			Body: file[0].buffer,
+			ContentType: file[0].memetype
+		};
+
+		s3.upload(params, function(s3Err, data) {
+			if (s3Err){
+				reject(s3Err);
+			}
+			console.log(`File uploaded successfully at ${data.Location}`);
+			resolve(data.Location);
+		});
+	});
+}
+
+
+module.exports = upload;
