1e6d335f0692413612ee8f0e093da90d5998a052
alvarian <vitarka88@gmail.com>
2020-08-18 20:09:56 -0400

    include file checker to defend s3 from non bufferable files, work on url display dom

--- api/aws_api/controllers/portal.js
+++ api/aws_api/controllers/portal.js
@@ -1,24 +1,26 @@
 const db = require('../config/db');
-const { upload, destroy } = require('../config/s3');
+const { s3Create, s3Destroy } = require('../config/s3');
 
 
 const createProject = async (req, res) => {
 	// create new image in s3, after success provide link for each to db to store for fields that require it
 	try {	
+		const { title, description, deployed_url, git_url, icon_url } = req.body;
+		const { game_file, style_file, icon_file } = req.files;
+
 		const bodyInputs = [	
-			req.body.title, 
-			req.body.description, 
-			req.body.deployed_url, 
-			await upload(req.files.game_file, `${req.body.title}/logic`), 
-			await upload(req.files.style_file, `${req.body.title}/style`),
-			req.body.git_url,
-			await upload(req.files.icon_file, `${req.body.title}/icon`)
+			title, 
+			description, 
+			deployed_url, 
+			await checkIfFileIsBufferable(game_file, `${title.replace(/\s/g, '')}/logic`), 
+			await checkIfFileIsBufferable(style_file, `${title.replace(/\s/g, '')}/style`),
+			git_url,
+			await checkIfFileIsBufferable(icon_file || icon_url, `${title.replace(/\s/g, '')}/icon`)
 		];
 
 		db.query(`SELECT * FROM public.add_project($1, $2, $3, $4, $5, $6, $7)`, bodyInputs,
 		(err, result) => {
 			if (err) throw err;
-
 		});
 	} catch (err) {
 		console.log('Failed to create project files in aws', err);
@@ -38,51 +40,64 @@ const readAllProjects = (req, res) => {
 	});
 };
 
-const readOneProject = (req, res) => {
-	db.query(`SELECT * FROM public.find_project_by_id(${req.params.id})`, (err, result) => {
-		console.log('read one', req.body);
+const checkIfFileIsBufferable = (file, awsKey) => {
+	return new Promise(async function(resolve, reject) {
+		// if field from update matches field from db, return
+		if (typeof file === 'string' || !file) {
+			return resolve(file);
+		}
 
-		res.json(result.rows[0]);
+		// if field does not match, pass parameter through s3Upload
+		let fileUrl = await s3Create(file, awsKey);
+		resolve(fileUrl);
 	});
 };
 
 const updateProject = async (req, res) => {
-	console.log('update', req.body);
-	
 	// if new logic, icon, or style, overwrite s3 and db fields
+	const { title, description, deployed_url, git_url, icon_url } = req.body;
+	const { game_file, style_file, icon_file } = req.files;
 
+	try {
+		const bodyInputs = [	
+			title, 
+			description, 
+			deployed_url, 
+			await checkIfFileIsBufferable(game_file, `${title.replace(/\s/g, '')}/logic`), 
+			await checkIfFileIsBufferable(style_file, `${title.replace(/\s/g, '')}/style`),
+			git_url,
+			await checkIfFileIsBufferable(icon_file || icon_url, `${title.replace(/\s/g, '')}/icon`)
+		];	
 
-
-
-
-
-	// const { title, description, type, git_url, icon_file } = req.body;
-	// db.query(`SELECT * FROM public.update_project(${req.params.id})`, (err, result) => {
-	// 	console.log(err ? err : result.rows);
-	// 	res.send(result.rows);
-	// });
-
-	res.redirect('/portal');
+		db.query(`SELECT * FROM public.update_project(${req.params.id}, $1, $2, $3, $4, $5, $6, $7)`, bodyInputs,
+		(err, result) => {
+			if (err) throw err
+		});
+	} catch (err) {
+		console.log('promise err from update', err)
+	} finally {
+		res.redirect('/portal');
+	}
 };
 
 const deleteProject = async (req, res) => {
 	try {
-		if (await destroy(`${req.body.title}/`)) {
-			db.query(`SELECT * FROM public.delete_project(${req.params.id})`, (err, result) => {
-				if (err) throw err
-
-				res.json({ msg: 'redirect plase' })
-			});
-		}
+		await s3Destroy(`${req.body.title.replace(/\s/g, '')}/`);
 	} catch (err) {
-		console.log('huh', err)
+		console.log('huh', err);
+	} finally {
+		db.query(`SELECT * FROM public.delete_project(${req.params.id})`, (err, result) => {
+			if (err) throw err
+
+			res.json({ msg: 'redirect plase' })
+		});
 	}
 };
 
 
 module.exports = {
 	createProject,
-	readAllProjects, readOneProject,
+	readAllProjects, 
 	updateProject,
 	deleteProject
 }
