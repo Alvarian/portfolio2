02c633042344b24544058f8b9e4b912357ee31cd
alvarian <vitarka88@gmail.com>
2020-08-20 00:32:16 -0400

    include cors, include smtp

--- api/client_api/routes/contact-routes.ts
+++ api/client_api/routes/contact-routes.ts
@@ -1,36 +1,36 @@
 import { Router } from "https://deno.land/x/oak@v6.0.1/mod.ts";
-import { client } from '../config/smtp.ts';
+import { client, EMAIL_U } from '../config/smtp.ts';
 
 
 const router = new Router();
 
 router.post('/api/contact', async ({ request, response }: { request: any, response: any }) => {
 	const body = await request.body();
-	const req = await body.value;
-	console.log(req)
-	// try {
-	// 	await client.send({
-	// 	  from: guestEmail, // Your Email address
-	// 	  to: EMAIL_U, // Email address of the destination
-	// 	  subject: "Message from portfolio!",
-	// 	  content: guestMsg,
-	// 	});
+	const { guestName, guestEmail, guestInqury } = await body.value;
+
+	try {
+		await client.send({
+		  from: guestEmail, // Your Email address
+		  to: EMAIL_U, // Email address of the destination
+		  subject: `Message from ${guestName}!`,
+		  content: `
+		  	<h2>${guestName}'s email: ${guestEmail}</h2>
+		  	${guestInqury}
+		  `,
+		});
 
 		response.status = 200;
 		response.body = {
 			success: true,
-			msg: req
+			msg: 'Successful Email'
 		};
-	// } catch (err) {
-	// 	console.log(err);
-	// 	response.status = 500;
-	// 	response.body = {
-	// 		success: false,
-	// 		msg: err
-	// 	}
-	// } finally {
-	// 	await client.close();
-	// }
+	} catch (err) {
+		response.status = 500;
+		response.body = {
+			success: false,
+			msg: err
+		}
+	} 
 });
 
 
--- api/client_api/server.ts
+++ api/client_api/server.ts
@@ -1,21 +1,16 @@
 import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
-import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.0/mod.ts";
+import projectRouter from './routes/project-routes.ts';
+import contactRouter from './routes/contact-routes.ts';
 
 
 const app = new Application();
 
-app.use(
-  oakCors({
-    origin: /^.+localhost:(3000)$/,
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }),
-);
+app.use(oakCors());
 
-import projectRouter from './routes/project-routes.ts';
 app.use(projectRouter.routes());
 app.use(projectRouter.allowedMethods());
 
-import contactRouter from './routes/contact-routes.ts';
 app.use(contactRouter.routes());
 app.use(contactRouter.allowedMethods());
 
