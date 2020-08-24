16c40068cea0b4212288d4f7f5b3b844d29fcc21
alvarian <vitarka88@gmail.com>
2020-08-24 11:58:36 -0400

    fixed on hover effects for app buttons in gallery, included synopsis field filler on app button mouseover

--- client/src/pages/components/ProjectList.js
+++ client/src/pages/components/ProjectList.js
@@ -7,8 +7,12 @@ function ProjectList(props) {
 	return props.data.map(project => (
 		<div key={project.id}>
 			<div className="dropCard">
-				<div style={{position: 'relative'}} onClick={props.callbackForModal.bind(this, {url: project.deployed_url, logic: project.game_file, style: project.style_file})}>
+				<div style={{position: 'relative'}} 
+					onClick={props.callbackForModal.bind(this, {url: project.deployed_url, logic: project.game_file, style: project.style_file})}
+					onMouseEnter={props.fillSynopsis.bind(this, project.title, project.description, project.game_file, project.deployed_url)}
+				>
 					<div style={{backgroundImage: "url("+ project.icon_file +")"}} className="card"></div>
+					
 					<div className="article lay">
 						<h2 className="clickOpen orbi">OPEN</h2>
 					</div>
