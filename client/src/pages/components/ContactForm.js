2d2f1a14edc2a6fba74190fd60cc64e0d08d1be6
alvarian <vitarka88@gmail.com>
2020-08-25 17:44:15 -0400

    include attrib datanetlify in contact form

--- client/src/pages/components/ContactForm.js
+++ client/src/pages/components/ContactForm.js
@@ -12,7 +12,7 @@ function ContactForm(props) {
 		<div style={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", backgroundImage: "url('images/img_highest.jpg')", minHeight: "100vh", minWidth: "300px", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundAttachment: "fixed"}}>
 			<h1 className="orbi">Contact</h1>
 
-			<form className="contactForm" onSubmit={props.HandleSubmitAndToggle(formValues)}>
+			<form name="contact" className="contactForm" onSubmit={props.HandleSubmitAndToggle(formValues)} data-netlify="true">
 				<input onChange={ e => formValues.guestName = e.target.value } className="orbi" style={{fontSize: "22pt",  color: "white", backgroundColor: "rgba(0,0,0,0.6)"}} name="name" type="text" placeholder="Your name" />
 				<input onChange={ e => formValues.guestEmail = e.target.value } className="orbi" style={{fontSize: "22pt",  color: "white", backgroundColor: "rgba(0,0,0,0.6)"}} name="email" type="email" placeholder="Your email" />
 				<textarea onChange={ e => formValues.guestInqury = e.target.value } className="orbi" style={{ color: "white", backgroundColor: "rgba(0,0,0,0.6)"}} name="message" id="" cols="30" rows="10" placeholder="Whats Up?"></textarea>
