8f15384962d783f2a56c9d629394576fed3e473f
alvarian <vitarka88@gmail.com>
2020-09-07 15:39:25 -0400

    STYLE: make resume body responsive through galaxy s5 and ipad horizontally and vertically while being regressional with desktop default

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -12,37 +12,12 @@ export default function Resume() {
 		<div className='resumeBackground'>
 			<Navbar class={'verdana resumeNav'} />
 
-			{width > 600 &&
-				<div style={{
-					position: 'absolute', 
-					transform: 'translate(40%, 150px)',
-					display: 'flex', 
-					flexDirection: 'column',
-					justifyContent: 'space-between', 
-					height: '120px'
-				}}>
-					<a data-tip="Go to full resume version located in google drive!" href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
-						<i className="fab bottomBtn fa-3x fa-google-drive"></i>
-					</a>
-
-					<a data-tip="Download displayed version(jpg)" style={{display: 'flex', justifyContent: 'center'}} href="./images/prettifiedResume9_20.jpg" download><i className="bottomBtn fas fa-3x fa-file-download"></i></a>
-					
-					<ReactTooltip />
-				</div>
-			}
-
-			<div style={{
-				display: 'flex', 
-				justifyContent: 'center', 
-				align: 'center',
-				overflow: 'hidden', 
-				height: '100vh'
-			}}>	
+			<div className='resumeBody'>	
 				<img className='prettyResume' src="./images/prettifiedResume9_20.jpg" alt="" />
 
 				{width <= 600 &&
 					<div className="resumeBottomBtns">
-						<a data-tip="Go to full resume version located in google drive!"href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
+						<a data-tip="Full resume version in google drive!" href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
 							<i className="bottomBtn fab fa-2x fa-google-drive"></i>
 						</a>
 
@@ -52,7 +27,25 @@ export default function Resume() {
 					</div>
 				}
 			</div>
+			
+			{width > 600 &&
+				<div style={{
+					position: 'fixed', 
+					transform: 'translate(60%, -550%)',
+					display: 'flex', 
+					flexDirection: 'column',
+					justifyContent: 'space-between', 
+					height: '120px'
+				}}>
+					<a data-tip="Full resume version in google drive!" href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
+						<i className="fab bottomBtn fa-3x fa-google-drive"></i>
+					</a>
 
+					<a data-tip="Download displayed version(jpg)" style={{display: 'flex', justifyContent: 'center'}} href="./images/prettifiedResume9_20.jpg" download><i className="bottomBtn fas fa-3x fa-file-download"></i></a>
+					
+					<ReactTooltip />
+				</div>
+			}
 		</div>
 	)
 }
