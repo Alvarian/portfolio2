6ff6e94f229f0a8fe9432622ce6b92ce59063383
alvarian <vitarka88@gmail.com>
2020-09-07 01:30:57 -0400

    in resume component include buttons for download and hyperlinking to google drive

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 import useResize from './components/hooks/useResize';
 import Navbar from './components/Navbar.js';
@@ -12,20 +13,45 @@ export default function Resume() {
 			<Navbar class={'verdana resumeNav'} />
 
 			{width > 600 &&
-				<a style={{position: 'absolute'}} href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
-					For all other qualifications please vist my Minified Resume
-				</a>
+				<div style={{
+					position: 'absolute', 
+					transform: 'translate(40%, 150px)',
+					display: 'flex', 
+					flexDirection: 'column',
+					justifyContent: 'space-between', 
+					height: '120px'
+				}}>
+					<a data-tip="Go to full resume version located in google drive!" href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
+						<i className="fab bottomBtn fa-3x fa-google-drive"></i>
+					</a>
+
+					<a data-tip="Download displayed version(jpg)" style={{display: 'flex', justifyContent: 'center'}} href="./images/prettifiedResume9_20.jpg" download><i className="bottomBtn fas fa-3x fa-file-download"></i></a>
+					
+					<ReactTooltip />
+				</div>
 			}
-			<div style={{display: 'flex', justifyContent: 'center', overflow: 'hidden', height: '100vh'}}>	
+
+			<div style={{
+				display: 'flex', 
+				justifyContent: 'center', 
+				align: 'center',
+				overflow: 'hidden', 
+				height: '100vh'
+			}}>	
 				<img className='prettyResume' src="./images/prettifiedResume9_20.jpg" alt="" />
 
 				{width <= 600 &&
-				<a style={{position: 'absolute'}} href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
-					For all other qualifications please vist my Minified Resume
-				</a>
-			}
-			</div>
+					<div className="resumeBottomBtns">
+						<a data-tip="Go to full resume version located in google drive!"href="https://docs.google.com/document/d/1E8sK0bPbW3zeLZb4_ld9EtZ4x_iNW_nqJhrPTvbSZW4/edit#">
+							<i className="bottomBtn fab fa-2x fa-google-drive"></i>
+						</a>
 
+						<a data-tip="Download displayed version(jpg)" style={{display: 'flex', justifyContent: 'center'}} href="./images/prettifiedResume9_20.jpg" download><i className="bottomBtn fas fa-2x fa-file-download"></i></a>
+			
+						<ReactTooltip />
+					</div>
+				}
+			</div>
 
 		</div>
 	)
